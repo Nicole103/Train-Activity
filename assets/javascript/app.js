a2a8661e45c16316f531554a6b10d2c43d5f5be4
Nicole Heersink <nheersink8@gmail.com>
2019-11-16 20:19:18 -0700

    fix for sending to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
+//Initialize Firebase
+var config = {
     apiKey: "AIzaSyCMGC7-jjw9YscrcVMOcJ-jnrzx33siBpU",
     authDomain: "train-scheduler-280bf.firebaseapp.com",
     databaseURL: "https://train-scheduler-280bf.firebaseio.com",
@@ -8,56 +8,88 @@ var firebaseConfig = {
     messagingSenderId: "339430017300",
     appId: "1:339430017300:web:7f1ae948dd7ec214995a01"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
-
-var database = firebase.database();
-
-$("#submitbtn").on("click",function(event){
-    
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  // Button for adding to the schedule trains
+  $("#add-train-btn").on("click", function (event) {
     event.preventDefault();
-
+  
+    // Grabs user input
     var trainName = $("#train-name-input").val().trim();
-    var trainDest = $("#train-destination-input").val().trim();
-    var firstTrain = moment($("#train-time-input").val().trim(),"HH:mm").subtract(10,"years").format("X");
-    var trainFreq = $("#train-frequency-input").val().trim();
- 
-
+    var trainDest = $("#dest-input").val().trim();
+    var firstTrain = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
+    var trainFreq = $("#freq-input").val().trim();
+  
+    // object to send to firebase
     var newTrain = {
-        name: trainName,
-        destination: trainDest,
-        frequency: trainFreq,
-        firstTrain: firstTrain,
-    }
-
+      name: trainName,
+      destination: trainDest,
+      firstTrain: firstTrain,
+      frequency: trainFreq
+    };
+  
+    // Uploads data to the database
     database.ref().push(newTrain);
-
-    alert("New Train Scheduled!");
-
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+  
+    alert("A train has been scheduled!");
+  
+    // Clears all of the text-boxes
     $("#train-name-input").val("");
-    $("#train-destination-input").val("");
-    $("#train-time-input").val("");
-    $("#train-frequency-input").val("");
+    $("#dest-input").val("");
+    $("#first-train-input").val("");
+    $("#freq-input").val("");
+  });
+  
+  // Create Firebase event for adding trains to the database 
+  database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().destination;
+    var firstTrain = childSnapshot.val().firstTrain;
+    var trainFreq = childSnapshot.val().frequency;
+  
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(firstTrain);
+    console.log(trainFreq);
+  
+    // Prettify the first train time
+    var firstTrainPretty = moment(firstTrain, "hh:mm");
+  
+    // var currentTime = moment();
 
-    return false;
+    // calculate the differenct between the first trian in minutes
+    var diffTimeInMin = moment().diff(moment(firstTrainPretty, "X"), "minutes");
+    console.log(diffTimeInMin);
 
-});
-
-database.ref().on("child_added", function(snapshot){
-
-    var name = snapshot.val().name;
-    var destination = snapshot.val().destination;
-    var frequency = snapshot.val().frequency;
-    var firstTrain = snapshot.val().firstTrain;
-
-    var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
-    var minutes = frequency - remainder;
-    var arrival = moment().add(minutes,"m").format("HH:mm");
+    var freqRemainingMin = diffTimeInMin % frequency;
+  
+    // 
+    var nextArrivalTime = moment().add((frequency - freqRemainingMin), "minutes");
+    console.log(nextArrivalTime);
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainFreq),
+      $("<td>").text(nextArrivalTime),
+      $("<td>").text(freqRemainingMin),
+     
+    );
+  
+    $("#schedule-table > tbody").append(newRow);
     
-    $("#trainScheduleTable > tbody").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td></tr>");
-
-
-});
-
-
+  });
+  
